3e00bfabc7ca03f50fdd9aa257868b226ca72352
lofitskyi <eduard.lofitskyi@nixsolutions.com>
2017-05-04 16:05:46 +0300

    Initial commit

---
+++ src/main/java/com/lofitskyi/AmzEbayPriceCheckApplication.java
@@ -0,0 +1,12 @@
+package com.lofitskyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzEbayPriceCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmzEbayPriceCheckApplication.class, args);
+	}
+}
